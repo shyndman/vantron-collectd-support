1b03cf40152ad89bfe00c372ee18c6775c310f05
Scott Hyndman <scotty.hyndman@gmail.com>
2025-03-08 08:32:13 -0500

    Add a command for installing collectd configuration

---
+++ src/vantron_collectd_support/collectd/install.py
@@ -0,0 +1,51 @@
+import importlib
+import importlib.resources
+import inspect
+import os
+import sys
+from pathlib import Path
+
+from loguru import logger
+
+import vantron_collectd_support as vantron_package
+
+from . import conf as conf_package
+
+logger.remove()
+logger.add(sys.stderr, colorize=True, format="<green>{elapsed}</green> <lvl>{level}</lvl> {message}")
+
+COLLECTD_CONFIG_RESOURCE_BASENAME = "vantron.collectd.conf"
+SRC_PATH_TEMPLATE_VAR_NAME = "vantron_package_src_path"
+VENV_PATH_TEMPLATE_VAR_NAME = "vantron_package_venv_packages_path"
+VENV_PATH_ENV_NAME = "VIRTUAL_ENV"
+COLLECTD_CONFIG_PATH = "/etc/collectd/collectd.conf.d/vantron.collectd.conf"
+
+
+def run():
+    logger.info("Installing CollectD plugin")
+
+    conf = importlib.resources.read_text(conf_package, COLLECTD_CONFIG_RESOURCE_BASENAME)
+    formatted_conf = conf.format(
+        **{
+            SRC_PATH_TEMPLATE_VAR_NAME: find_src_dir().as_posix(),
+            VENV_PATH_TEMPLATE_VAR_NAME: os.getenv(VENV_PATH_ENV_NAME),
+        }
+    )
+
+    write_conf(formatted_conf)
+
+
+def write_conf(formatted_conf):
+    logger.info(f"Config:\n\n{formatted_conf}")
+    try:
+        logger.info(f"Writing to {COLLECTD_CONFIG_PATH}")
+        with open(COLLECTD_CONFIG_PATH, "w") as f:
+            f.write(formatted_conf)
+    except PermissionError:
+        logger.exception(f"Cannot write to {COLLECTD_CONFIG_PATH}")
+        logger.error("Try:")
+        logger.error("sudo -E `which uv` run install-collectd-plugin")
+
+
+def find_src_dir() -> Path:
+    return Path(inspect.getfile(vantron_package)).parent.resolve()
